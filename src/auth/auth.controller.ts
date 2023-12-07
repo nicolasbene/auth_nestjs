ef9f6350b68f1461cbed3c49415dad444f99a46b
nicolasbene <nibenoit@student.42.fr>
2023-12-07 18:44:46 +0100

    installation du packet nest en dev dependancy + erreur 404 pas compris

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+
+@Controller('auth')
+export class AuthController{
+	constructor(private authService: AuthService) {}
+
+	@Post('signup')
+	signup() {
+		return 'I am signed up'
+	}
+	@Post('signin')
+	signin() {
+		return 'I am signed in'
+	}
+}
